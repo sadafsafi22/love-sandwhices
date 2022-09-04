0a7cdcfef3fa89ca7f3b8b02c0a6858b5174dd62
sadafsafi22 <safisadaf96@gmail.com>
2022-09-04 17:04:00 +0000

    Refactor update worksheet function into one

--- run.py
+++ run.py
@@ -59,31 +59,21 @@ def validate_data(values):
         
     return True
 
-def update_sales_worksheet(data):
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the list data provided.
+    Receives a list of integers to be inserted into a worksheet
+    Update the relewant worksheet with the data provided
     """
-
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-def update_surplus_worksheet(new_surplus_data):
-    """
-    updating surplus worksheet by adding new surplus data
-    """
-
-    print("Updating Surplus worksheet... \n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(new_surplus_data)
-    print("Surplus worksheet updated successfully.\n")
+    print(f"Updating {worksheet} worksheet... \n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully \n")
 
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate  the surplus for each item type.
 
-    The Surplus is defined as the sales figure subtracted from  the stuck:
+    The Surplus is defined as the sales figure subtracted from  the stuck
     -Positive surplis indicates waste.
     -Negative surplis indicates extra made when stock was sold out.
     """
@@ -111,11 +101,11 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data,"sales")
     calculate_surplus_data(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     # print(new_surplus_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data,"surplus")
 
 
 
